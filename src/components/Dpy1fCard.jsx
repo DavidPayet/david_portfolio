0ce50ef054ddc2f6f6ebe898b5ecbd9db8501b49
DavidPayet <davidpayet570@gmail.com>
2020-03-11 00:09:22 +0100

    add 2PY1F app

---
+++ src/components/Dpy1fCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  NavLink
+} from 'reactstrap';
+
+import './Card.scss';
+
+const Dpy1fCard = () => {
+  return (
+    <div className="Card">
+      <NavLink href="https://2py1f.netlify.com">
+      <Card className="card">
+        <CardImg top width="100%" src="/medias/dpy1f2.png" alt="2py1f-img" />
+        <CardBody>
+          <CardTitle className="title" >2PY1F</CardTitle>
+
+        </CardBody>
+      </Card>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Dpy1fCard;
