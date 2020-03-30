14c7496fa5ae10f2bd0c8b9f660e65ec471dfb7e
DavidPayet <davidpayet570@gmail.com>
2020-03-30 20:42:15 +0200

    add tooltip

--- src/components/ChessPlayersCard.jsx
+++ src/components/ChessPlayersCard.jsx
@@ -6,21 +6,26 @@ import {
   CardTitle,
   NavLink
 } from 'reactstrap';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import './Card.scss';
+
+const longText = `Voici une vidéo de démonstration de Chess Player Data. Cette application recupère des données sur des joueurs d'échecs avec la possibilité d'aujout et de suppression grâce à des requêtes SQL. Utilisation de NodeJS pour le backend.`;
 
 const ChessPlayersCard = () => {
   return (
     <div className="Card">
-      <NavLink href="/chessplayers">
-        <Card className="card">
-          <CardImg top width="100%" src="/medias/chessPlayers-img.png" alt="chessPlayers-img" />
-          <CardBody>
-            <CardTitle className="title" >Chess Players</CardTitle>
-          </CardBody>
-        </Card>
-      </NavLink>
-    </div>
+      <Tooltip arrow title={longText} TransitionProps={{ timeout: 600 }}>
+        <NavLink href="/chessplayers">
+          <Card className="card">
+            <CardImg top width="100%" src="/medias/chessPlayers-img.png" alt="chessPlayers-img" />
+            <CardBody>
+              <CardTitle className="title" >Chess Players</CardTitle>
+            </CardBody>
+          </Card>
+        </NavLink>
+      </Tooltip>
+    </div >
   );
 };
 
--- src/components/Dpy1fCard.jsx
+++ src/components/Dpy1fCard.jsx
@@ -6,21 +6,26 @@ import {
   CardTitle,
   NavLink
 } from 'reactstrap';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import './Card.scss';
+
+const longText = `Voici un poste de télécommunication révolutionnaire nommé 2PY1F ! Connectez vous et Essayez le en y laissant un message ! Vous avez aussi la possibilité de suppimer votre message ainsi que d'aimer un message existant. Cette aplication codée en react utilise firebase pour l'authentification et la base de données.`;
 
 const Dpy1fCard = () => {
   return (
     <div className="Card">
-      <NavLink href="https://2py1f.netlify.com">
-      <Card className="card">
-        <CardImg top width="100%" src="/medias/dpy1f2.png" alt="2py1f-img" />
-        <CardBody>
-          <CardTitle className="title" >2PY1F</CardTitle>
+      <Tooltip arrow title={longText} TransitionProps={{ timeout: 600 }}>
+        <NavLink href="https://2py1f.netlify.com">
+          <Card className="card">
+            <CardImg top width="100%" src="/medias/dpy1f2.png" alt="2py1f-img" />
+            <CardBody>
+              <CardTitle className="title" >2PY1F</CardTitle>
 
-        </CardBody>
-      </Card>
-      </NavLink>
+            </CardBody>
+          </Card>
+        </NavLink>
+      </Tooltip>
     </div>
   );
 };
