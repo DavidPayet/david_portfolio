91e13972fc6ef5160d93687900e972ac8f955a67
DavidPayet <davidpayet570@gmail.com>
2020-04-25 15:07:18 +0200

    add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from "react-scroll";
+
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="Header">
+      <Container className="header">
+        <Row>
+          <Col xl="12" lg="12" md="12" sm="12" xs="12">
+            <div className="upPage">
+              <img src="/medias/picture5.jpg" alt="background" className="img" />
+              <div className="titre">
+                <h1 className="lightSpeedIn">
+                  David Payet
+              </h1>
+                <br />
+                <h2 className="zoomInUp">
+                  Web Developer
+              </h2>
+              </div>
+
+            </div>
+            <Row className="bottom-arrow">
+            <Col xl="12" lg="12" md="12" sm="12" xs="12">
+              <Link
+                activeClass="active"
+                to="about-section"
+                spy={true}
+                smooth={true}
+                offset={70}
+                duration={500}
+              >
+                <span >&#x2303;</span>
+              </Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Header;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
 import NavBar from '../NavBar';
+import Header from '../Header';
 import About from '../About';
 import Competences from '../Competences';
 import Portfolio from '../Portfolio';
 import Contact from '../Contact';
 import Footer from '../Footer';
-import { Link } from "react-scroll";
 import BackToTopButton from '../BackToTopButton';
 import './HomePage.scss';
 
@@ -14,39 +13,7 @@ const HomePage = () => {
   return (
     <div className="HomePage" id="up-page">
       <NavBar />
-      <Container className="homePage">
-        <Row>
-          <Col xl="12" lg="12" md="12" sm="12" xs="12">
-            <div className="upPage">
-              <img src="/medias/picture5.jpg" alt="background" className="img" />
-              <div className="titre">
-                <h1 className="lightSpeedIn">
-                  David Payet
-              </h1>
-                <br />
-                <h2 className="zoomInUp">
-                  Web Developer
-              </h2>
-              </div>
-
-            </div>
-            <Row className="bottom-arrow">
-            <Col xl="12" lg="12" md="12" sm="12" xs="12">
-              <Link
-                activeClass="active"
-                to="about-section"
-                spy={true}
-                smooth={true}
-                offset={70}
-                duration={500}
-              >
-                <span >&#x2303;</span>
-              </Link>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
+      <Header />
       <About />
       <Competences />
       <Portfolio />
