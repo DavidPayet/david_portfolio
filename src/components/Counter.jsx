6b7f55923ceceb5312cc1eff870e214ee0822cfe
DavidPayet <davidpayet570@gmail.com>
2020-05-06 16:03:44 +0200

    counter with hooks

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,52 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 
 import './Counter.scss';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      result: 0,
-    }
-    this.decrement = this.decrement.bind(this);
-    this.increment = this.increment.bind(this);
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  const decrement = () => {
+    setCount(count - 1)
   }
 
-  decrement() {
-    this.setState({
-      result: this.state.result - 1
-    })
+  const increment = () => {
+    setCount(count + 1)
   }
 
-  increment() {
-    this.setState({
-      result: this.state.result + 1
-    })
-  }
-
-  render() {
-    return (
-      <div className="Counter">
-        <Container className="counter">
-              <h2>Counter</h2>
-              <span className="result">{this.state.result}</span>
-          <Row className="row">
-            <Col xl="4" lg="4" md="4" sm="2" xs="2" />
-            <Col xl="2" lg="2" md="2" sm="4" xs="4">
-              {/*eslint-disable-next-line*/}
-              <button onClick={this.decrement}>👎🏽</button>
-            </Col>
-            <Col xl="2" lg="2" md="2" sm="4" xs="4">
-              {/*eslint-disable-next-line*/}
-              <button onClick={this.increment}>👍🏽</button>
-            </Col>
-            <Col xl="4" lg="4" md="4" sm="2" xs="2" />
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <div className="Counter">
+      <Container className="counter">
+        <h2>Counter</h2>
+        <span className="result">{count}</span>
+        <Row className="row">
+          <Col xl="4" lg="4" md="4" sm="2" xs="2" />
+          <Col xl="2" lg="2" md="2" sm="4" xs="4">
+            <AiFillCaretDown
+              onClick={decrement}
+              className="btn"
+            />
+          </Col>
+          <Col xl="2" lg="2" md="2" sm="4" xs="4">
+            <AiFillCaretUp
+              onClick={increment}
+              className="btn"
+            />
+          </Col>
+          <Col xl="4" lg="4" md="4" sm="2" xs="2" />
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Counter;
