9821849982d7b1c025064f9c2c42a1007dc1063c
DavidPayet <davidpayet570@gmail.com>
2020-06-08 22:08:50 +0200

    add link button for cv

--- src/components/Competences.jsx
+++ src/components/Competences.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { FiDownload } from 'react-icons/fi';
 
 import '../styles/Competences.scss';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 const Competences = () => {
+  const classes = useStyles();
   return (
     <div className="Competences" id="competences-section">
       <Container className="competences">
@@ -21,6 +33,15 @@ const Competences = () => {
             <img className="logos" src="/medias/logos/logo-mysql.png" alt="logo-mysql" />
             <img className="logos" src="/medias/logos/logo-sql.png" alt="logo-sql" />
           </Col>
+        </Row>
+        <Row>
+          <Col xl="1" lg="1" md="1" sm="2" xs="2" />
+          <Col className={classes.root} xl="10" lg="10" md="10" sm="8" xs="8">
+            <Button variant="outlined" href="/cv">
+              <FiDownload style={{ fontSize: "20px", marginRight: "10px" }} /> Télécharger mon CV
+              </Button>
+          </Col>
+          <Col xl="1" lg="1" md="1" sm="2" xs="2" />
         </Row>
       </Container>
     </div>
