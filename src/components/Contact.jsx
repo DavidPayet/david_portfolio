510ea5180af8668ee23dde7600755a587c7f11a9
DavidPayet <davidpayet570@gmail.com>
2020-08-06 16:13:09 +0200

    recaptcha test5

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -81,9 +81,11 @@ export default class Contact extends React.Component {
                       onChange={this.handleChange}
                     />
                   </div>
-                  <div data-netlify-recaptcha="true" name="recaptcha"></div>
+                  <p>
+                    <div data-netlify-recaptcha="true" name="recaptcha"></div>
+                  </p>
                 </div>
-                <Button data-netlify-recaptcha="true" variant="outlined" type="submit">Envoyer</Button>
+                <Button variant="outlined" type="submit">Envoyer</Button>
               </form>
             </Col>
             <Col className="info" xl="6" lg="6" md="6" sm="12" xs="12" >
