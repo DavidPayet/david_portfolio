107fac55f68dde7f4804a28c77248e3de2b21cbc
DavidPayet <davidpayet570@gmail.com>
2020-08-07 21:52:34 +0200

    remove HomePage.scss

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,8 +8,6 @@ import Header from '../components/Header';
 import NavBar from '../components/NavBar';
 import Portfolio from '../components/Portfolio';
 
-import '../styles/HomePage.scss';
-
 const HomePage = () => {
   return (
     <div className="HomePage" id="up-page">
